82a9f550a30d0d48f5092f525496d3eb8473ed73
Luis Otavio <luispim2005@gmail.com>
2026-03-17 10:30:41 -0300

    initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <>
+      <div className="flex bg-black min-w-96">
+        <button>
+          <h2>Home</h2>
+        </button>
+        <button>
+          <h2>Friends</h2>
+        </button>
+        <button>
+          <h2>Profile</h2>
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <h1>Recem Assistidos</h1>
+      // aqui vai os cards
+      <h1>Sugestões</h1>
+      // aqui vai os cards
+      <h1>Seus Amigos Viram</h1>
+      // aqui vai os cards
+    </>
+  );
+}
+
+export default Home;
